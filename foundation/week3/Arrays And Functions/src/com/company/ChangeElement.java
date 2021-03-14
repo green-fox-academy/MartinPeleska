5ad33fa4fda1584f15f0f0f690cc8436dbf11092
Martin Peleska <peleska.martin@seznam.cz>
2021-03-14 18:11:17 +0100

    Add Array's exercises

---
+++ foundation/week3/Arrays And Functions/src/com/company/ChangeElement.java
@@ -0,0 +1,19 @@
+package com.company;
+
+// - Create an array variable named `numbers`
+//   with the following content: `[1, 2, 3, 8, 5, 6]`
+// - Change the value of the fourth element (8) to 4
+// - Print the fourth element
+
+
+public
+class ChangeElement {
+
+    public static void main (String[]arg) {
+        int[] numbers = {1, 2, 3, 8, 5, 6};
+        System.out.println(numbers[3]);
+        numbers[3] = 4;
+        System.out.println(numbers[3]);
+
+    }
+}
