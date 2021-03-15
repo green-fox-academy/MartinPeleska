af4dda04d5ecec7506dffc0e1336d709a986382b
Martin Peleska <peleska.martin@seznam.cz>
2021-03-15 16:07:49 +0100

    Add Array's exercises

---
+++ foundation/week3/Arrays And Functions/src/ApendA.java
@@ -0,0 +1,22 @@
+// - Create an array variable named `animals`
+//   with the following content:
+//   `["koal", "pand", "zebr", "anacond", "bo", "chinchill", "cobr", "gorill",
+//     "hyen", "hydr", "iguan", "impal", "pum", "tarantul", "pirahn"]`
+//
+// - Add an `"a"` at the end of all elements (use a loop!)
+
+
+public class ApendA {
+
+    public static void main (String[] args){
+        String[] animals ={"koal", "pand", "zebr", "anacond", "bo", "chinchill", "cobr", "gorill",
+                "hyen", "hydr", "iguan", "impal", "pum", "tarantul", "pirahn"};
+
+        for (String i : animals) {
+            System.out.println(i+"a");
+        }
+
+    }
+    }
+
+
---
+++ foundation/week3/Arrays And Functions/src/SwapElements.java
@@ -0,0 +1,23 @@
+// - Create an array variable named `orders`
+//   with the following content: `["first", "second", "third"]`
+// - Swap the first and the third element of `orders`
+
+public class SwapElements {
+    public static void main (String[] args ){
+        String[] orders = { "first", "second", "third" }; {
+            String x = orders[2];
+            orders[2] = orders[0];
+            orders[0] = x;
+
+            System.out.println(orders[0]);
+            System.out.println(orders[2]);
+
+        }
+    }
+}
+/*    String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+cars[0] = "Opel";
+        System.out.println(cars[0]);
+// Now outputs Opel instead of Volvo
+
+ */
